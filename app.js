f375db21ddfbf6ba6d55f18faca685ef4089926a
Damienlucas <lucas.damien@free.fr>
2021-10-18 21:07:11 +0200

    feat:amelioration bouton jouer modal

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ var myInput = document.getElementById('myInput')
 
 // ----------------------------------------------jeu-----------------------------------------------------
 // ---------------------------------------------choix------------------------------------------------
+const photoOriginal = document.getElementById('photo-original');
+const photoModifiee = document.getElementById('photo-modifiee');
 const jouerSteampunk = document.getElementById('jouer-steampunk');
 const jouerChinaTown = document.getElementById('jouer-chinaTown');
 const contBoxPhotos = document.getElementById('cont-box-photos');
 const choix = document.getElementById('choix');
 const menuAccueil = document.getElementById('menu-accueil');
+const solution = document.getElementById('solution');
+const items = document.getElementsByClassName('items');
 var progressBar = document.querySelector('.progress-bar');
 var decouvert
 var choixDifficulte
@@ -23,53 +27,59 @@ var morceau7;
 var morceau8;
 var morceau9;
 var morceau10;
+var jouer = false;
 
 menuAccueil.addEventListener('click', () => {
     document.location.reload(true);
 })
+solution.addEventListener('click', () => {
+    // photoModifiee.style.opacity = "1";
+    photoModifiee.style.backgroundColor = "blue";
+})
 
 function option(){
 jouerSteampunk.addEventListener("click", function() {
-    contBoxPhotos.style.display = "block";
-    choix.style.display = "none";
-    choixDifficulte = 10;
-    decouvert = 0;
-    morceau1 = false;
-    morceau2 = false;
-    morceau3 = false;
-    morceau4 = false;
-    morceau5 = false;
-    morceau6 = false;
-    morceau7 = false;
-    morceau8 = false;
-    morceau9 = false;
-    morceau10 = false;
-    steampunk();
+    if(jouer==false){
+        contBoxPhotos.style.display = "block";
+        choix.style.display = "none";
+        choixDifficulte = 10;
+        decouvert = 0;
+        morceau1 = false;
+        morceau2 = false;
+        morceau3 = false;
+        morceau4 = false;
+        morceau5 = false;
+        morceau6 = false;
+        morceau7 = false;
+        morceau8 = false;
+        morceau9 = false;
+        morceau10 = false;
+        jouer = true;
+        steampunk();
+    }
 })
 jouerChinaTown.addEventListener("click", function() {
-    contBoxPhotos.style.display = "block";
-    choix.style.display = "none";
-    choixDifficulte = 10;
-    decouvert = 0;
-    morceau1 = false;
-    morceau2 = false;
-    morceau3 = false;
-    morceau4 = false;
-    morceau5 = false;
-    morceau6 = false;
-    morceau7 = false;
-    morceau8 = false;
-    morceau9 = false;
-    morceau10 = false;
-    chinaTown();
+    if(jouer==false){
+        contBoxPhotos.style.display = "block";
+        choix.style.display = "none";
+        choixDifficulte = 10;
+        decouvert = 0;
+        morceau1 = false;
+        morceau2 = false;
+        morceau3 = false;
+        morceau4 = false;
+        morceau5 = false;
+        morceau6 = false;
+        morceau7 = false;
+        morceau8 = false;
+        morceau9 = false;
+        morceau10 = false;
+        jouer = true;
+        chinaTown();
+    }
 })
 }
 option();
-
-
-
-const photoOriginal = document.getElementById('photo-original');
-const photoModifiee = document.getElementById('photo-modifiee');
 
 // --------------------------------------------------progress bar----------------------------------
 var scoreValeur = document.getElementById('score-valeur');
@@ -105,52 +115,62 @@ photoModifiee.appendChild(imgSteampunkModif);
 boutArme.setAttribute('src', './images/steampunk/bout-arme.png');
 boutArme.setAttribute('alt', 'bout de l arme');
 boutArme.id = "bout-arme";
+boutArme.className = "items";
 photoModifiee.appendChild(boutArme);
 
 
 boutCeinture.setAttribute('src', './images/steampunk/bouton-ceinture.png');
 boutCeinture.setAttribute('alt', 'bout de l arme');
 boutCeinture.id = "bouton-ceinture";
+boutCeinture.className = "items";
 photoModifiee.appendChild(boutCeinture);
 
 coeur.setAttribute('src', './images/steampunk/coeur.png');
 coeur.setAttribute('alt', 'coeur du bustier');
 coeur.id = "coeur";
+coeur.className = "items";
 photoModifiee.appendChild(coeur);
 
 fils.setAttribute('src', './images/steampunk/fils-electriques.png');
 fils.setAttribute('alt', 'fils éléctriques');
 fils.id = "fils-electriques";
+fils.className = "items";
 photoModifiee.appendChild(fils);
 
 gachette.setAttribute('src', './images/steampunk/gachette.png');
 gachette.setAttribute('alt', 'gachette');
 gachette.id = "gachette";
+gachette.className = "items";
 photoModifiee.appendChild(gachette);
 
 lacet.setAttribute('src', './images/steampunk/lacet.png');
 lacet.setAttribute('alt', 'lacet de droite');
 lacet.id = "lacet";
+lacet.className = "items";
 photoModifiee.appendChild(lacet);
 
 medaille.setAttribute('src', './images/steampunk/medaille.png');
 medaille.setAttribute('alt', 'morceau de medaille');
 medaille.id = "medaille";
+medaille.className = "items";
 photoModifiee.appendChild(medaille);
 
 lacetRajout.setAttribute('src', './images/steampunk/rajout-lacet.png');
 lacetRajout.setAttribute('alt', 'rajout de lacet à gauche');
 lacetRajout.id = "rajout-lacet";
+lacetRajout.className = "items";
 photoModifiee.appendChild(lacetRajout);
 
 boutRajout.setAttribute('src', './images/steampunk/rajout-bouton.png');
 boutRajout.setAttribute('alt', 'rajout d un bouton à gauche');
 boutRajout.id = "rajout-bouton";
+boutRajout.className = "items";
 photoModifiee.appendChild(boutRajout);
 
 verresLunette.setAttribute('src', './images/steampunk/verres-lunettes.png');
 verresLunette.setAttribute('alt', 'verres des lunettes');
 verresLunette.id = "verres-lunettes";
+verresLunette.className = "items";
 photoModifiee.appendChild(verresLunette);
 
 boutArme.addEventListener("click", function() {
@@ -294,52 +314,62 @@ function chinaTown(){
     cones.setAttribute('src', './images/china town/cones.png');
     cones.setAttribute('alt', 'cones de traveaux');
     cones.id = "cones";
+    cones.className = "items";
     photoModifiee.appendChild(cones);
     
     
     dame.setAttribute('src', './images/china town/dame.png');
     dame.setAttribute('alt', 'dame');
     dame.id = "dame";
+    dame.className = "items";
     photoModifiee.appendChild(dame);
     
     ecureuil.setAttribute('src', './images/china town/ecureuil.png');
     ecureuil.setAttribute('alt', 'ecureuil');
     ecureuil.id = "ecureuil";
+    ecureuil.className = "items";
     photoModifiee.appendChild(ecureuil);
     
     forza.setAttribute('src', './images/china town/forza.png');
     forza.setAttribute('alt', 'enseigne forza');
     forza.id = "forza";
+    forza.className = "items";
     photoModifiee.appendChild(forza);
     
     photomaton.setAttribute('src', './images/china town/photomaton.png');
     photomaton.setAttribute('alt', 'affiche du photomaton');
     photomaton.id = "photomaton";
+    photomaton.className = "items";
     photoModifiee.appendChild(photomaton);
     
     pigeon.setAttribute('src', './images/china town/pigeon.png');
     pigeon.setAttribute('alt', 'pigeon');
     pigeon.id = "pigeon";
+    pigeon.className = "items";
     photoModifiee.appendChild(pigeon);
     
     pomme.setAttribute('src', './images/china town/pomme.png');
     pomme.setAttribute('alt', 'pomme');
     pomme.id = "pomme";
+    pomme.className = "items";
     photoModifiee.appendChild(pomme);
     
     t.setAttribute('src', './images/china town/t.png');
     t.setAttribute('alt', 'enseigne t');
     t.id = "t";
+    t.className = "items";
     photoModifiee.appendChild(t);
     
     tel.setAttribute('src', './images/china town/tel.png');
     tel.setAttribute('alt', 'numéro de téléphone');
     tel.id = "tel";
+    tel.className = "items";
     photoModifiee.appendChild(tel);
     
     cableGauche.setAttribute('src', './images/china town/cable-gauche.png');
     cableGauche.setAttribute('alt', 'cable à gauche de la photo');
     cableGauche.id = "cable-gauche";
+    cableGauche.className = "items";
     photoModifiee.appendChild(cableGauche);
     
     cones.addEventListener("click", function() {
